9b219c09b21c5ed39dd600ed018d3a5eb9d19c0e
u1and0 <e01.ando60@gmail.com>
2023-03-22 10:43:27 +0900

    [refactor] CV Character Voice class spin out

--- voicevox_audio.py
+++ voicevox_audio.py
@@ -10,7 +10,6 @@ fast=Trueオプションで高速URLを使用します。
 確認の仕方はcheck_point()
 """
 import os
-import sys
 from io import BytesIO
 import json
 from typing import Union
@@ -19,142 +18,12 @@ import argparse
 import requests
 from pydub import AudioSegment
 from pydub.playback import play
+from voicevox_character import CV
 
 apikey = os.getenv("VOICEVOX_API_KEY")
 url = "https://api.tts.quest/v1"
 fast_url = "https://api.su-shiki.com/v2"
 local_url = "http://localhost:50021"
-
-
-class CV(IntEnum):
-    """VOICEVOX Characters Voice IDs
-
-    $ curl -fsSL 'https://api.su-shiki.com/v2/voicevox/speakers?key={API_KEY}' | jq
-    [
-      {
-        "supported_features": {
-          "permitted_synthesis_morphing": "SELF_ONLY"
-        },
-        "name": "四国めたん",
-        "speaker_uuid": "7ffcb7ce-00ec-4bdc-82cd-45a8889e43ff",
-        "styles": [
-          {
-            "name": "ノーマル",
-            "id": 2
-          },
-          {
-            "name": "あまあま",
-            "id": 0
-          },
-          ...
-
-    Python用に整形
-    $ curl -fsSL 'https://api.su-shiki.com/v2/voicevox/speakers?key={API_KEY}' |
-        jq '.[] | "\(.name)\(.styles[].name) = \(.styles[].id)"'
-    """
-
-    四国めたんあまあま = 0
-    四国めたんノーマル = 2
-    四国めたんセクシー = 4
-    四国めたんツンツン = 6
-    四国めたんささやき = 36
-    四国めたんヒソヒソ = 37
-
-    ずんだもんノーマル = 3
-    ずんだもんあまあま = 1
-    ずんだもんツンツン = 7
-    ずんだもんセクシー = 5
-    ずんだもんささやき = 22
-    ずんだもんヒソヒソ = 38
-
-    春日部つむぎノーマル = 8
-    雨晴はうノーマル = 10
-    波音リツノーマル = 9
-
-    玄野武宏ノーマル = 11
-    玄野武宏喜び = 39
-    玄野武宏ツンギレ = 40
-    玄野武宏悲しみ = 41
-
-    白上虎太郎ふつう = 12
-    白上虎太郎わーい = 32
-    白上虎太郎びくびく = 33
-    白上虎太郎おこ = 34
-    白上虎太郎びえーん = 35
-
-    青山龍星ノーマル = 13
-    冥鳴ひまりノーマル = 14
-    九州そらノーマル = 16
-    九州そらあまあま = 15
-    九州そらツンツン = 18
-    九州そらセクシー = 17
-    九州そらささやき = 19
-
-    もち子さんノーマル = 20
-    剣崎雌雄ノーマル = 21
-
-    WhiteCULノーマル = 23
-    WhiteCULたのしい = 24
-    WhiteCULかなしい = 25
-    WhiteCULびえーん = 26
-
-    後鬼人間ver = 27
-    後鬼ぬいぐるみver = 28
-
-    No7ノーマル = 29
-    No7アナウンス = 30
-    No7読み聞かせ = 31
-
-    ちび式じいノーマル = 42
-
-    櫻歌ミコノーマル = 43
-    櫻歌ミコ第二形態 = 44
-    櫻歌ミコロリ = 45
-
-    小夜SAYOノーマル = 46
-
-    ナースロボ＿タイプＴノーマル = 47
-    ナースロボ＿タイプＴ楽々 = 48
-    ナースロボ＿タイプＴ恐怖 = 49
-    ナースロボ＿タイプＴ内緒話 = 50
-
-    聖騎士紅桜ノーマル = 51
-    雀松朱司ノーマル = 52
-    麒ヶ島宗麟ノーマル = 53
-
-    def __str__(self):
-        """
-        >>> CV.四国めたんノーマル
-        四国めたんノーマル
-        """
-        return self.name
-
-    def __repr__(self):
-        return self.__str__()
-
-    @classmethod
-    def table(cls):
-        """CV一覧
-        ID: キャラクター名
-        WARNING: IDに抜けがあるとValueErrorで一覧表示できない
-        """
-        dic = {}
-        i = 0
-        while True:
-            try:
-                dic[i] = CV(i)
-                i += 1
-            except ValueError:
-                break
-        return dic
-
-    @classmethod
-    def from_string(cls, value):
-        """
-        >>> CV(2)
-        四国めたんノーマル
-        """
-        return cls[value].name
 
 
 class Mode(IntEnum):
@@ -240,9 +109,11 @@ def play_voice(text,
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="VOICEVOX CV")
-    parser.add_argument("-s", "--speaker", default=0,
+    parser.add_argument("-s",
+                        "--speaker",
+                        default=0,
                         help="VOICEVOX Character voice ID")
-    parser.add_argument("text",  help="VOICEVOXに話させる文字列")
+    parser.add_argument("text", help="VOICEVOXに話させる文字列")
     args = parser.parse_args()
     # リクエスト過多の429エラーが出たときには
     # fastバージョンを使う
---
+++ voicevox_character.py
@@ -0,0 +1,142 @@
+from enum import IntEnum, auto
+
+
+class CV(IntEnum):
+    """VOICEVOX Characters Voice IDs
+
+    $ curl -fsSL 'https://api.su-shiki.com/v2/voicevox/speakers?key={API_KEY}'
+        | jq
+    [
+      {
+        "supported_features": {
+          "permitted_synthesis_morphing": "SELF_ONLY"
+        },
+        "name": "四国めたん",
+        "speaker_uuid": "7ffcb7ce-00ec-4bdc-82cd-45a8889e43ff",
+        "styles": [
+          {
+            "name": "ノーマル",
+            "id": 2
+          },
+          {
+            "name": "あまあま",
+            "id": 0
+          },
+          ...
+
+    Python用に整形
+    $ curl -fsSL 'https://api.su-shiki.com/v2/voicevox/speakers?key={API_KEY}'
+        | jq '.[] | "\(.name)\(.styles[].name) = \(.styles[].id)"'
+
+
+    Usage:
+        >>> CV["ずんだもんあまあま"] == 1
+        True
+        >>> int(CV["ずんだもんあまあま"] )
+        1
+        >>> CV(2)
+        四国めたんノーマル
+    """
+
+    四国めたんあまあま = 0
+    四国めたんノーマル = 2
+    四国めたんセクシー = 4
+    四国めたんツンツン = 6
+    四国めたんささやき = 36
+    四国めたんヒソヒソ = 37
+
+    ずんだもんノーマル = 3
+    ずんだもんあまあま = 1
+    ずんだもんツンツン = 7
+    ずんだもんセクシー = 5
+    ずんだもんささやき = 22
+    ずんだもんヒソヒソ = 38
+
+    春日部つむぎノーマル = 8
+    雨晴はうノーマル = 10
+    波音リツノーマル = 9
+
+    玄野武宏ノーマル = 11
+    玄野武宏喜び = 39
+    玄野武宏ツンギレ = 40
+    玄野武宏悲しみ = 41
+
+    白上虎太郎ふつう = 12
+    白上虎太郎わーい = 32
+    白上虎太郎びくびく = 33
+    白上虎太郎おこ = 34
+    白上虎太郎びえーん = 35
+
+    青山龍星ノーマル = 13
+    冥鳴ひまりノーマル = 14
+    九州そらノーマル = 16
+    九州そらあまあま = 15
+    九州そらツンツン = 18
+    九州そらセクシー = 17
+    九州そらささやき = 19
+
+    もち子さんノーマル = 20
+    剣崎雌雄ノーマル = 21
+
+    WhiteCULノーマル = 23
+    WhiteCULたのしい = 24
+    WhiteCULかなしい = 25
+    WhiteCULびえーん = 26
+
+    後鬼人間ver = 27
+    後鬼ぬいぐるみver = 28
+
+    No7ノーマル = 29
+    No7アナウンス = 30
+    No7読み聞かせ = 31
+
+    ちび式じいノーマル = 42
+
+    櫻歌ミコノーマル = 43
+    櫻歌ミコ第二形態 = 44
+    櫻歌ミコロリ = 45
+
+    小夜SAYOノーマル = 46
+
+    ナースロボ＿タイプＴノーマル = 47
+    ナースロボ＿タイプＴ楽々 = 48
+    ナースロボ＿タイプＴ恐怖 = 49
+    ナースロボ＿タイプＴ内緒話 = 50
+
+    聖騎士紅桜ノーマル = 51
+    雀松朱司ノーマル = 52
+    麒ヶ島宗麟ノーマル = 53
+
+    def __str__(self):
+        """
+        >>> CV.四国めたんノーマル
+        四国めたんノーマル
+        """
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+    @classmethod
+    def table(cls):
+        """CV一覧
+        ID: キャラクター名
+        WARNING: IDに抜けがあるとValueErrorで一覧表示できない
+        """
+        dic = {}
+        i = 0
+        while True:
+            try:
+                dic[i] = CV(i)
+                i += 1
+            except ValueError:
+                break
+        return dic
+
+    @classmethod
+    def from_string(cls, value):
+        """
+        >>> CV(2)
+        四国めたんノーマル
+        """
+        return cls[value].name
